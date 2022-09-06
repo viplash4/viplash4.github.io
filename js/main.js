f514b0d5771aa168678761d09d40cde385d88163
Max Zalubovskiy <44732505+viplash4@users.noreply.github.com>
2022-09-06 15:09:53 +0300

    Add files via upload
    some fixes

---
+++ js/main.js
@@ -0,0 +1,53 @@
+/*1 slide bg animated*/
+
+
+
+window.addEventListener("scroll", function(){
+    let slides = Array.from(this.document.querySelectorAll(".slide"))
+    let offset = window.pageYOffset;
+    slides.forEach((slide1)=>{
+        slide1.style.backgroundPositionY = offset * 0.1 + "px";
+    })
+    //slider.style.backgroundPositionY = offset * 0.1 + "px";
+}
+)
+/*text sliding*/
+function reveal() {
+    var reveals = document.querySelectorAll(".slide-in");
+  
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+}
+  
+let slideIndex = 1;
+showSlides(slideIndex);
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("slides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
